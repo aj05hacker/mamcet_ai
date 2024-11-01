50706fca6eaf3eed31273eefc25294950250d592
ABDUL HAJEES <84639525+aj05hacker@users.noreply.github.com>
2024-11-01 19:53:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,11 +2,18 @@ from flask import Flask, request, jsonify
 import os
 import google.generativeai as genai
 from dotenv import load_dotenv
+import firebase_admin
+from firebase_admin import credentials, firestore
 
 # Load environment variables
 load_dotenv()
 GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
 genai.configure(api_key=GOOGLE_API_KEY)
+
+# Initialize Firebase
+cred = credentials.Certificate(os.getenv('FIREBASE_CREDENTIALS'))
+firebase_admin.initialize_app(cred)
+db = firestore.client()
 
 app = Flask(__name__)
 
